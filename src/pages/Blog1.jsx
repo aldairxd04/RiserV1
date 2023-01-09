e32a021460b334640933fe487e0d11267520de5a
aldairxd04 <114270094+aldairxd04@users.noreply.github.com>
2023-01-09 14:43:39 -0600

    Add files via upload

---
+++ src/pages/Blog1.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Blogs from '../components/Blogs'
+import Footer from '../components/Footer'
+import NavBar from '../components/NavBar'
+
+const Blog1 = () => {
+  return (
+    <>
+        <NavBar/>
+        <Blogs/>
+        <Footer/>
+    </>
+  )
+}
+
+export default Blog1
---
+++ src/pages/ContactFormPage.jsx
@@ -0,0 +1,16 @@
+
+import ContactForm from '../components/ContactForm'
+import Footer from '../components/Footer'
+import NavBar from '../components/NavBar'
+
+const ContactFormPage = () => {
+  return (
+    <>
+        <NavBar/>
+        <ContactForm/>
+        <Footer/>
+    </>
+  )
+}
+
+export default ContactFormPage
