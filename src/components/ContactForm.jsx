aa6dfbf8e4d6b5d5ea832ab8e304de2ccbaef538
aldairxd04 <114270094+aldairxd04@users.noreply.github.com>
2023-01-09 13:49:19 -0600

    Add files via upload

---
+++ src/components/ContactForm.jsx
@@ -0,0 +1,197 @@
+import { useState } from 'react'
+import { Switch } from '@headlessui/react'
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+
+export default function ContactForm() {
+  const [agreed, setAgreed] = useState(false)
+
+  return (
+    <div className="overflow-hidden bg-[#F1F0F0] pt-32 md:pt-0 py-16 px-6 lg:px-8 lg:pb-24 lg:pt-32">
+      <div className="relative mx-auto max-w-xl">
+        <svg
+          className="absolute left-full translate-x-1/2 transform"
+          width={404}
+          height={404}
+          fill="none"
+          viewBox="0 0 404 404"
+          aria-hidden="true"
+        >
+          <defs>
+            <pattern
+              id="85737c0e-0916-41d7-917f-596dc7edfa27"
+              x={0}
+              y={0}
+              width={20}
+              height={20}
+              patternUnits="userSpaceOnUse"
+            >
+              <rect x={0} y={0} width={4} height={4} className="text-gray-200" fill="currentColor" />
+            </pattern>
+          </defs>
+          <rect width={404} height={404} fill="url(#85737c0e-0916-41d7-917f-596dc7edfa27)" />
+        </svg>
+        <svg
+          className="absolute right-full bottom-0 -translate-x-1/2 transform"
+          width={404}
+          height={404}
+          fill="none"
+          viewBox="0 0 404 404"
+          aria-hidden="true"
+        >
+          <defs>
+            <pattern
+              id="85737c0e-0916-41d7-917f-596dc7edfa27"
+              x={0}
+              y={0}
+              width={20}
+              height={20}
+              patternUnits="userSpaceOnUse"
+            >
+              <rect x={0} y={0} width={4} height={4} className="text-gray-200" fill="currentColor" />
+            </pattern>
+          </defs>
+          <rect width={404} height={404} fill="url(#85737c0e-0916-41d7-917f-596dc7edfa27)" />
+        </svg>
+        <div className="text-center">
+          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Escríbenos</h2>
+        </div>
+        <div className="mt-12">
+          <form action="#" method="POST" className="grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-8">
+            <div>
+              <label htmlFor="first-name" className="block text-sm font-medium text-gray-700">
+                Nombre
+              </label>
+              <div className="mt-1">
+                <input
+                  type="text"
+                  name="first-name"
+                  id="first-name"
+                  autoComplete="given-name"
+                  className="block w-full rounded-md border-gray-300 py-3 px-4 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
+                />
+              </div>
+            </div>
+            <div>
+              <label htmlFor="last-name" className="block text-sm font-medium text-gray-700">
+                Apellido
+              </label>
+              <div className="mt-1">
+                <input
+                  type="text"
+                  name="last-name"
+                  id="last-name"
+                  autoComplete="family-name"
+                  className="block w-full rounded-md border-gray-300 py-3 px-4 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
+                />
+              </div>
+            </div>
+            <div className="sm:col-span-2">
+              <label htmlFor="company" className="block text-sm font-medium text-gray-700">
+                Empresa
+              </label>
+              <div className="mt-1">
+                <input
+                  type="text"
+                  name="company"
+                  id="company"
+                  autoComplete="organization"
+                  className="block w-full rounded-md border-gray-300 py-3 px-4 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
+                />
+              </div>
+            </div>
+            <div className="sm:col-span-2">
+              <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+                Correo electrónico
+              </label>
+              <div className="mt-1">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  className="block w-full rounded-md border-gray-300 py-3 px-4 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
+                />
+              </div>
+            </div>
+            <div className="sm:col-span-2">
+              <label htmlFor="phone-number" className="block text-sm font-medium text-gray-700">
+                Teléfono
+              </label>
+              <div className="relative mt-1 rounded-md shadow-sm">
+                <input
+                  type="text"
+                  name="phone-number"
+                  id="phone-number"
+                  autoComplete="tel"
+                  className="block w-full rounded-md border-gray-300 py-3 px-4 focus:border-indigo-500 focus:ring-indigo-500"
+                  
+                />
+              </div>
+            </div>
+            <div className="sm:col-span-2">
+              <label htmlFor="message" className="block text-sm font-medium text-gray-700">
+                Mensaje
+              </label>
+              <div className="mt-1">
+                <textarea
+                  id="message"
+                  name="message"
+                  rows={4}
+                  className="block w-full rounded-md border-gray-300 py-3 px-4 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
+                  defaultValue={''}
+                />
+              </div>
+            </div>
+            {/* <div className="sm:col-span-2">
+              <div className="flex items-start">
+                <div className="flex-shrink-0">
+                  <Switch
+                    checked={agreed}
+                    onChange={setAgreed}
+                    className={classNames(
+                      agreed ? 'bg-indigo-600' : 'bg-gray-200',
+                      'relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2'
+                    )}
+                  >
+                    <span className="sr-only">Agree to policies</span>
+                    <span
+                      aria-hidden="true"
+                      className={classNames(
+                        agreed ? 'translate-x-5' : 'translate-x-0',
+                        'inline-block h-5 w-5 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out'
+                      )}
+                    />
+                  </Switch>
+                </div>
+                <div className="ml-3">
+                  <p className="text-base text-gray-500">
+                    By selecting this, you agree to the{' '}
+                    <a href="#" className="font-medium text-gray-700 underline">
+                      Privacy Policy
+                    </a>{' '}
+                    and{' '}
+                    <a href="#" className="font-medium text-gray-700 underline">
+                      Cookie Policy
+                    </a>
+                    .
+                  </p>
+                </div>
+              </div>
+            </div> */}
+            <div className="sm:col-span-2">
+              <button
+                type="submit"
+                className="inline-flex w-full items-center justify-center rounded-md border border-transparent bg-[#001E57] px-6 py-3 text-base font-medium text-gray-300 shadow-sm hover:bg-[#FF6900] focus:outline-none focus:ring-2 focus:ring-[#001E57] focus:ring-offset-2"
+              >
+                Enviar
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  )
+}
