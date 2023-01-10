3a188ad61c368b5751e2fa77bfbf98bad50a936b
aldairxd04 <114270094+aldairxd04@users.noreply.github.com>
2023-01-10 17:10:16 -0600

    Add files via upload

---
+++ src/components/HSServices.jsx
@@ -0,0 +1,38 @@
+import { Link } from "react-router-dom"
+
+const navigation = [
+  { name: 'Product', href: '#' },
+  { name: 'Features', href: '#' },
+  { name: 'Marketplace', href: '#' },
+  { name: 'Company', href: '#' },
+]
+
+export default function HSServices() {
+
+  return (
+    <div className="isolate bg-cover bg-center bg-[url('/assets/risr7.jpg')]/black-50">
+      
+      <main>
+        <div className="relative px-6 lg:px-8">
+          <div className=" max-w-3xl pt-20 pb-32 sm:pt-48 sm:pb-40">
+            <div className='flex flex-col md:text-center'>
+              
+
+                <h1 className=" mt-10 text-4xl font-bold text-black md:text-[#FF6900] uppercase tracking-tight sm:text-center sm:text-5xl">
+                 El único sistema de pruebas de esfuerzos para sistemas Top Drive en América Latina.
+                </h1>
+                <Link to="/contacto"
+                className="inline-flex items-center mt-5 md:ml-8 md:w-1/5 w-1/2 text-center md:px-8 px-10 py-3 border text-sm font-medium rounded-full 
+                shadow-sm text-slate-900 border-[#FF6900] bg-white hover:border-[#2DCCD3] hover:bg-[#2DCCD3] focus:outline-none focus:ring-2 focus:ring-offset-2
+                focus:ring-[#2DCCD3] transition duration-500">
+                  <p className="m-auto">Contáctanos</p>
+                </Link>
+
+              
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+  )
+}
---
+++ src/components/HeaderContact.jsx
@@ -0,0 +1,17 @@
+export default function HeaderContact() {
+  return (
+    <div className="py-24 bg-center bg-no-repeat bg-cover  bg-[url('/assets/sirisr.png')]">
+      <div className="mx-auto max-w-7xl py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
+        <div className="text-center py-10">
+          
+          <p className="mt-1 text-4xl font-bold tracking-tight text-white sm:text-5xl lg:text-6xl">
+            Contactanos.
+          </p>
+          <p className="mx-auto mt-5 max-w-xl text-xl text-white">
+          Nuestro compromiso es asesorar oportunamente a nuestros clientes para que tomen la mejor desición en seguros. 
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
