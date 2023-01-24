3005edb44d276c812f90478b3137a4504f9eda36
aldairxd04 <114270094+aldairxd04@users.noreply.github.com>
2023-01-24 13:26:18 -0600

    Update CompareSlider.jsx

--- src/components/CompareSlider.jsx
+++ src/components/CompareSlider.jsx
@@ -11,7 +11,7 @@ const CompareSlider = () => {
         <ReactCompareSliderImage src="https://www.shutterstock.com/image-photo/top-drive-drillerkey-260nw-494964490.jpg" srcSet="https://www.shutterstock.com/image-photo/top-drive-drillerkey-260nw-494964490.jpg" alt="Image one" />
       }
       itemTwo={
-        <ReactCompareSliderImage src="http://3.bp.blogspot.com/-GZJ1ee3nnxY/UVIcJb8ip_I/AAAAAAAAATA/ds9LeECgI8c/s1600/IMG_0866%5B1%5D.JPG" srcSet="http://3.bp.blogspot.com/-GZJ1ee3nnxY/UVIcJb8ip_I/AAAAAAAAATA/ds9LeECgI8c/s1600/IMG_0866%5B1%5D.JPG" alt="Image two" />
+        <ReactCompareSliderImage src="https://apex.eg/wp-content/uploads/2021/04/top-1.jpg" srcSet="https://apex.eg/wp-content/uploads/2021/04/top-1.jpg" alt="Image two" />
       }
     />
     </div>
