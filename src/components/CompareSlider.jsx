a6ea5bc42f353c35b2b616d0c209701b117c8a04
aldairxd04 <114270094+aldairxd04@users.noreply.github.com>
2023-01-30 15:40:53 -0600

    Update CompareSlider.jsx

--- src/components/CompareSlider.jsx
+++ src/components/CompareSlider.jsx
@@ -5,13 +5,13 @@ import {
 
 const CompareSlider = () => {
   return (
-    <div className="relative mx-auto max-w-3xl py-20">
+    <div className="relative mx-auto max-w-xl py-20">
         <ReactCompareSlider
       itemOne={
         <ReactCompareSliderImage src="https://www.shutterstock.com/image-photo/top-drive-drillerkey-260nw-494964490.jpg" srcSet="https://www.shutterstock.com/image-photo/top-drive-drillerkey-260nw-494964490.jpg" alt="Image one" />
       }
       itemTwo={
-        <ReactCompareSliderImage src="/assets/risr8.jpg" srcSet="/assets/risr8.jpg" alt="Image two" />
+        <ReactCompareSliderImage src="/assets/risrA.jpg" srcSet="/assets/risrA.jpg" alt="Image two" />
       }
     />
     </div>
